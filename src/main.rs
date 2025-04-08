2bd4a38e97e75120525f695751a0cb24da701cb6
valentin994 <valentin.vareskic@gmail.com>
2025-04-08 11:21:47 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+use axum::{routing, Json, Router};
+use icalendar::{Calendar, CalendarComponent, Component};
+use reqwest::{get, StatusCode};
+use serde::Serialize;
+use tower_http::cors::{CorsLayer, Any};
+
+#[derive(Serialize, Debug)]
+struct ReservedDates {
+    start_date: String,
+    end_date: String,
+}
+
+async fn get_apartment_ics() -> (StatusCode, Json<Vec<ReservedDates>>) {
+    let mut dates: Vec<ReservedDates> = Vec::new();
+    let response = get("https://www.airbnb.com/calendar/ical/819570085784640178.ics?s=52e6735d7bb44c1b6b8750562c78dcae")
+        .await
+        .unwrap()
+        .text()
+        .await;
+
+    let parsed_calendar: Calendar = response.unwrap().parse().unwrap();
+
+    for component in &parsed_calendar.components {
+        if let CalendarComponent::Event(event) = component {
+            dates.push(ReservedDates {
+                start_date: event.get_start().unwrap().date_naive().to_string(),
+                end_date: event.get_end().unwrap().date_naive().to_string(),
+            });
+        }
+    }
+    (StatusCode::OK, Json(dates))
+}
+
+#[tokio::main]
+async fn main() {
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods(Any)
+        .allow_methods(Any)
+        .allow_headers(Any);
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+    let app = Router::new()
+        .route("/", routing::get(|| async { "Hello world" }))
+        .route("/get_dates", routing::get(get_apartment_ics))
+        .layer(cors);
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    tracing::info!("Starting up server on port 3000");
+    axum::serve(listener, app).await.unwrap();
+}
